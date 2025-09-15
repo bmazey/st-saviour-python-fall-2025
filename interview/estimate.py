21d19621c508cc00bb4fffaef9ac07a7c4199b82
ebucci <ebucci@stsaviour.org>
2025-09-15 09:25:14 -0400

    estimate.py

--- interview/estimate.py
+++ interview/estimate.py
@@ -4,7 +4,10 @@ def rounder(number: float) -> int:
     round accepts a float and returns a rounded integer.
     the number is rounded up iff the decimal is >= .5
     """
+    integer_part = int(number)
+    decimal_part = number - integer_part
 
-    # TODO implement round function
-
-    return 0
+    if decimal_part >= 0.5:
+        return integer_part + 1
+    else:
+        return integer_part
