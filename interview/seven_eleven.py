b87cfa8b8e6188cbd6502f75be9607f05979aa29
ebucci <ebucci@stsaviour.org>
2025-09-16 10:02:54 -0400

    seven_eleven.py

--- interview/seven_eleven.py
+++ interview/seven_eleven.py
@@ -7,7 +7,9 @@ def seven_eleven(number: int) -> str:
         - 'seveneleven' if the number is a multiple of 7 and 11
         - an empty string if the number is not a multiple of 7 or 11
     """
-
-    # TODO implement seven_eleven function
-
-    return ''
+    result = ''
+    if number % 7 == 0:
+        result += 'seven'
+    if number % 11 == 0:
+        result += 'eleven'
+    return result
