0a92e8a0ffab123c29db88d3f2844105887864cc
ebucci <ebucci@stsaviour.org>
2025-09-17 14:17:54 -0400

    test_password.py

--- interview/test_password.py
+++ interview/test_password.py
@@ -1,27 +1,24 @@
+import string
 from password import generate_password
 
-
 def test_password_alpha_characters():
-    # TODO BONUS ensure that the first 5 characters are letters
-    pass
+    password = generate_password()
+    assert password[:5].isalpha(), "First 5 characters should be alphabetic"
 
 def test_password_numberic_characters():
-    # TODO BONUS ensure the placement of the 4 digit characters
-    pass
+    password = generate_password()
+    assert password[5:9].isdigit(), "Characters 6 to 9 should be digits"
 
 def test_password_symbol_character():
-    # TODO BONUS ensure the final character is a symbol
-    pass
+    password = generate_password()
+    symbols = string.punctuation
+    assert password[-1] in symbols, "Last character should be a symbol"
 
 def test_password_length():
-    # TODO BONUS ensure the length of the password is 10
-    pass
+    password = generate_password()
+    assert len(password) == 10, "Password length should be 10"
 
 def test_password_unique():
-    # HINT we ignore collisions for the purposes of this exercise
-    # TODO BONUS create two passwords, and ensure they are distinct
-    pass
-
-def contains(s: str, collection: list):
-    # check if any characters in collection are present in s
-    return 1 in [c in s for c in collection]
+    password1 = generate_password()
+    password2 = generate_password()
+    assert password1 != password2, "Passwords should be unique"
