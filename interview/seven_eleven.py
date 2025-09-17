a75554963524dedf9740a0ae8e925fa1630f68d5
jcodesup-35 <jadaphilpnerdy@gmail.com>
2025-09-17 14:29:53 -0400

    seveneleven done

--- interview/seven_eleven.py
+++ interview/seven_eleven.py
@@ -8,6 +8,11 @@ def seven_eleven(number: int) -> str:
         - an empty string if the number is not a multiple of 7 or 11
     """
 
-    # TODO implement seven_eleven function
-
-    return ''
+    if number % 7 == 0:
+        return 'seven'
+    if number % 11 == 0:
+        return 'eleven'
+    if number % 7 and number % 11 == 0:
+        return 'seveneleven'
+    else:
+        return ''
