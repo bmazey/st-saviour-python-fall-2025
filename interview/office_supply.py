91b629daf187785b769507ec239bdbc9d96651e4
chelsea <cnicolas@stsaviour.org>
2025-09-17 14:28:56 -0400

    seveneleven done

--- interview/office_supply.py
+++ interview/office_supply.py
@@ -5,9 +5,9 @@ def staple_to_front(s: str, c: str) -> str:
       - ex: staple_to_front('saviour', 'st. ') -> 'st. saviour'
     """
 
-    # TODO
-
     return ''
+  
+
 
 def staple_to_end(s: str, c: str) -> str:
     """
