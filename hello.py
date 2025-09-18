6ba23a27f1eeb0be963ccb79ddfb21fc1896f1d9
Victoria <vickyvictorious525@gmail.com>
2025-09-18 09:31:00 -0400

    seven eleven

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 
 if __name__ == '__main__':
-    print('Welcome to AP CS: Principles!')
+
+ print('Welcome to AP CS: Principles!')
+
+ a = 'saviour'
+ b = 'st.'
+ if a and b:
+  print (a + b)
+
--- interview/estimate.py
+++ interview/estimate.py
@@ -4,7 +4,14 @@ def rounder(number: float) -> int:
     round accepts a float and returns a rounded integer.
     the number is rounded up iff the decimal is >= .5
     """
+    
+    decimal = number - int(number)
 
-    # TODO implement round function
+    if decimal >= 0.5: 
+       result = int(number) + 1 # add 1 to round up decimal because the decimal is greater than .5
+    else:
+        result = int(number) 
 
-    return 0
+    return result
+
+  
