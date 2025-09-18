3e07898d7ae1e370ad33eafec8750c296db9b383
Jordan <jgpmo2008@gmail.com>
2025-09-18 09:33:49 -0400

    lol done

--- interview/office_supply.py
+++ interview/office_supply.py
@@ -27,7 +27,7 @@ def shred_first_character(s: str) -> str:
 
     # TODO
 
-    return 's[;-1]'
+    return s[1:]
 
 def shred_last_character(s: str) -> str:
     """
@@ -37,4 +37,4 @@ def shred_last_character(s: str) -> str:
 
     # TODO
 
-    return ''
+    return s[:-1]
