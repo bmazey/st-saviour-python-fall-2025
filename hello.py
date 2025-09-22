0096ab9bd4650be7d12590830457b9e765eeddb0
Jordan <jgpmo2008@gmail.com>
2025-09-22 13:26:05 -0400

    ok

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
+import random
 
 if __name__ == '__main__':
-    print('Welcome to AP CS: Principles!')
-    s = 'september'
-    print(s[::])
+    #print('Welcome to AP CS: Principles!')
+    
+    r = random.randint(0,10)
+    print('the random number is: ' + str(r))
