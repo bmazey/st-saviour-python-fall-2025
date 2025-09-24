3e73be416805def9ee67b21bcd8f1f04b22e6ed0
afre <afre@stsaviour.org>
2025-09-24 10:08:06 -0400

    looking good

--- interview/estimate.py
+++ interview/estimate.py
@@ -6,5 +6,8 @@ def rounder(number: float) -> int:
     """
 
     # TODO implement round function
+    decimal = number % 1
+    if decimal >= 0.5:
+        return int(number) + 1
 
-    return 0
+    return int(number)
