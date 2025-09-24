050d12df7e9690393a08a835e0e0ba01627fd5d9
jcodesup-35 <jadaphilpnerdy@gmail.com>
2025-09-24 09:58:08 -0400

    office supply done entirely

--- interview/office_supply.py
+++ interview/office_supply.py
@@ -5,7 +5,7 @@ def staple_to_front(s: str, c: str) -> str:
       - ex: staple_to_front('saviour', 'st. ') -> 'st. saviour'
     """
 
-    result = s + c 
+    result = c + s  #c must come before s when concatenating because order is flipped
     return result #concatenating the two strings together 
 
 def staple_to_end(s: str, c: str) -> str:
@@ -14,7 +14,7 @@ def staple_to_end(s: str, c: str) -> str:
       - ex: staple_to_front('st. ', 'saviour') -> 'st. saviour'
     """
 
-    result = c + s 
+    result = s + c  #s must come before c when concatenating two strings that are in order
     return result #concatenating but switching the order of s and c - c + s
 
 def shred_first_character(s: str) -> str:
@@ -23,9 +23,7 @@ def shred_first_character(s: str) -> str:
       - ex: shred_first_character('sst. saviour') -> 'st. saviour'
     """
 
-    # TODO
-
-    return ''
+    return s[1: ]  #includes the entire string besides the first character
 
 def shred_last_character(s: str) -> str:
     """
@@ -33,6 +31,4 @@ def shred_last_character(s: str) -> str:
       - ex: shred_first_character('st. saviourr') -> 'st. saviour'
     """
 
-    # TODO
-
-    return ''
+    return s[ :-1]  #includes the entire string besides the last character
