b96f9a09884c6fa27133f5efc50bd0e36ff1e664
brandon mazey <brandon@Mac.localdomain>
2025-10-08 09:31:10 -0400

    examples

--- hello.py
+++ hello.py
@@ -1,3 +1,25 @@
 
 if __name__ == '__main__':
     print('Welcome to AP CS: Principles!')
+
+    i = 0
+    while i < 100:
+        if i == 13:
+            print('the number 13 is unlucky ...')
+            i += 1
+            continue
+        print('I will not cheat on my CS exam ... ' + str(i))
+        i += 1
+
+    # for i in range(5, 100, 2):
+    #     print('I will not cheat on my CS exam ... ' + str(i))
+
+    s = 'October'
+    # i = 0
+    # print(s[6])
+    # while i < len(s):
+    #     print('the character we\'re on is: ' + s[i])
+    #     i += 1
+    
+    # for c in s:
+    #     print('the character we\'re on is: ' + c)
