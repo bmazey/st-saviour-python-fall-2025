9acd339af37353001e96f9f040c9c6f2f68bfa4b
jcodesup-35 <jadaphilpnerdy@gmail.com>
2025-10-10 12:59:54 -0400

    demo in class

--- hello.py
+++ hello.py
@@ -3,3 +3,14 @@ if __name__ == '__main__':
     # Hi from Jada
     # starting lab 2
     print('Welcome to AP CS: Principles!')
+
+    numbers = [3, 14, 7, 88, 7]
+    i = 0
+    while i < len(numbers):
+        print('index: ' + str(i) + ' content: ' + str(numbers[i]))
+        i += 1
+
+    numbers = [3, 14, 7, 88, 7, 11]
+    for x in numbers:
+        print(x)
+
