6983ad8bdf8fe7e3e1161429155193217b7fb9ed
mostolaza-gif <mostolaza@stsaviour.org>
2025-10-14 08:13:24 -0400

    huybuhyu

--- interview/test_office_supply.py
+++ interview/test_office_supply.py
@@ -3,17 +3,17 @@ from office_supply import staple_to_front, staple_to_end, shred_first_character,
 
 # Define a function to test the 'stapler' functions.
 def test_stapler():
-    # Verify that the staple_to_front function works correctly.
-    # The 'assert' keyword checks if a condition is true. If it's not, the test fails.
-    # Test case: stapling 'p' to the front of 'anda' should result in 'panda'.
+    # verify that the"  staple_to_front "function works correctly 
+    # thne 'assert' keyword checks if a condition is true. If it's not then the test fails 
+    #  stapling 'p' to the front of 'anda' should result in 'panda' 
     assert staple_to_front('anda', 'p') == 'panda'
-    # Test case: stapling 's' to the front of 'aviour' should result in 'saviour'.
+    # stapling 's' to the front of 'aviour' should result in 'saviour'.
     assert staple_to_front('aviour', 's') == 'saviour'
 
     # Verify that the staple_to_end function works correctly.
-    # Test case: stapling 'a' to the end of 'pand' should result in 'panda'.
+    # stapling 'a' to the end of 'pand' should result in 'panda'.
     assert staple_to_end('pand', 'a') == 'panda'
-    # Test case: stapling 'r' to the end of 'saviou' should result in 'saviour'.
+    #  r stapling 'r' to the end of 'saviou' should result in 'saviour'.
     assert staple_to_end('saviou', 'r') == 'saviour'
 
 # Define a function to test the 'shredder' functions.
