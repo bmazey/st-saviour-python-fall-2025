14592af6740038807cfd24ce40860bb0d6059584
gomeze10 <gomezellianna2010@gmail.com>
2025-10-27 14:27:13 -0400

    .

--- hello.py
+++ hello.py
@@ -2,9 +2,3 @@
 if __name__ == '__main__':
     # Jordan is sooooo cool
     print('Welcome to AP CS: Principles!')
-
-letters = ['p', 'a', 'n', 'd', 'a'] 
-i = len(letters) - 1 
-while i >= 0:
-    print(letters[i])
-    i -= 1 
